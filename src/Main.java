b5d442934c4684206bc0547900fc2b51fdc41fdf
Oguz Ozsahin <oguzozshn@gmail.com>
2026-03-12 22:11:47 +0300

    Decent version

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,117 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         ArrayList<Vehicle> vehicles = new ArrayList<>();
 
+        boolean programRunning = true;
 
-        while (true) {
-            System.out.println("Please enter your brand (press 0 to quit):");
-            String userBrand = scanner.nextLine();
+        while (programRunning) {
+            System.out.println("\n=== MAIN MENU ===");
+            System.out.println("1 - Add Vehicle");
+            System.out.println("2 - Speed Up Vehicles");
+            System.out.println("3 - Show Vehicle Info");
+            System.out.println("0 - Quit");
+            System.out.print("Choose: ");
+            String mainChoice = scanner.nextLine();
 
-            if (userBrand.equals("0")) {
-                break;
+            switch (mainChoice) {
+                case "1":
+                    while (true) {
+                        System.out.println("\n--- ADD VEHICLE MENU ---");
+                        System.out.println("1 - Add Car");
+                        System.out.println("2 - Add Bicycle");
+                        System.out.println("0 - Back to Main Menu");
+                        System.out.print("Choose: ");
+                        String addChoice = scanner.nextLine();
+
+                        if (addChoice.equals("0")) {
+                            break;
+                        }
+
+                        System.out.print("Enter brand: ");
+                        String brand = scanner.nextLine();
+
+                        System.out.print("Enter model: ");
+                        String model = scanner.nextLine();
+
+                        if (addChoice.equals("1")) {
+                            vehicles.add(new Car(brand, model));
+                            System.out.println("Car added successfully.");
+                        } else if (addChoice.equals("2")) {
+                            vehicles.add(new Bicycle(brand, model));
+                            System.out.println("Bicycle added successfully.");
+                        } else {
+                            System.out.println("Invalid choice.");
+                        }
+                    }
+                    break;
+
+                case "2":
+                    while (true) {
+                        System.out.println("\n--- SPEED UP MENU ---");
+                        if (vehicles.isEmpty()) {
+                            System.out.println("No vehicles found.");
+                            break;
+                        }
+
+                        for (int i = 0; i < vehicles.size(); i++) {
+                            System.out.print((i + 1) + " - ");
+                            vehicles.get(i).ShowInfo();
+                        }
+
+                        System.out.println("0 - Back to Main Menu");
+                        System.out.print("Choose vehicle number: ");
+                        String vehicleChoice = scanner.nextLine();
+
+                        if (vehicleChoice.equals("0")) {
+                            break;
+                        }
+
+                        int index = Integer.parseInt(vehicleChoice) - 1;
+
+                        if (index >= 0 && index < vehicles.size()) {
+                            System.out.print("Enter speed up amount: ");
+                            String amount = scanner.nextLine();
+                            int amountInt = Integer.parseInt(amount);
+
+                            vehicles.get(index).SpeedUp(amountInt);
+
+                        } else {
+                            System.out.println("Invalid vehicle number.");
+                        }
+                    }
+                    break;
+
+                case "3":
+                    while (true) {
+                        System.out.println("\n--- SHOW INFO MENU ---");
+                        if (vehicles.isEmpty()) {
+                            System.out.println("No vehicles found.");
+                        } else {
+                            for (int i = 0; i < vehicles.size(); i++) {
+                                System.out.print((i + 1) + " - ");
+                                vehicles.get(i).ShowInfo();
+                            }
+                        }
+
+                        System.out.println("0 - Back to Main Menu");
+                        System.out.print("Choose 0 to return: ");
+                        String backChoice = scanner.nextLine();
+
+                        if (backChoice.equals("0")) {
+                            break;
+                        } else {
+                            System.out.println("Invalid choice.");
+                        }
+                    }
+                    break;
+
+                case "0":
+                    programRunning = false;
+                    System.out.println("Program closed.");
+                    break;
+
+                default:
+                    System.out.println("Invalid choice.");
             }
-
-            System.out.println("Please enter your model:");
-            String userModel = scanner.nextLine();
-
-            System.out.println("Choose vehicle type: 1-Car, 2-Bicycle");
-            String choice = scanner.nextLine();
-
-            if (choice.equals("1")) {
-                vehicles.add(new Car(userBrand, userModel));
-            } else if (choice.equals("2")) {
-                vehicles.add(new Bicycle(userBrand, userModel));
-            } else {
-                System.out.println("Invalid vehicle type.");
-            }
-        }
-
-        System.out.println("\nAdded vehicles:");
-        for (Vehicle vehicle : vehicles) {
-            vehicle.ShowInfo();
         }
 
         scanner.close();
@@ -57,7 +141,7 @@ public class Main {
 
         public void SpeedUp() {
             this.Speed += 10;
-            System.out.println("Vehicle speed has increased 10 units. New Speed: " + this.Speed);
+            System.out.println("No amount has entered - 1Vehicle speed has increased 10 units. New Speed: " + this.Speed);
         }
 
         public void SpeedUp(int amount) {
@@ -66,7 +150,7 @@ public class Main {
         }
 
         public void ShowInfo() {
-            System.out.println("Vehicle: " + getBrand() + " " + Model);
+            System.out.println("Vehicle: " + getBrand() + " " + Model + " | Speed: " + Speed);
         }
     }
 
@@ -80,7 +164,7 @@ public class Main {
         }
 
         public void ShowInfo() {
-            System.out.println("Car: " + getBrand() + " " + Model);
+            System.out.println("Car: " + getBrand() + " " + Model + " | Speed: " + Speed);
         }
     }
 
@@ -94,7 +178,7 @@ public class Main {
         }
 
         public void ShowInfo() {
-            System.out.println("Bicycle: " + getBrand() + " " + Model);
+            System.out.println("Bicycle: " + getBrand() + " " + Model + " | Speed: " + Speed);
         }
     }
 }
